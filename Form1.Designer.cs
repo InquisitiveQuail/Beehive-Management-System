6b4f6719b916004e71412509ad0e30a5c3f67e2e
InquisitiveQuail <mylloyd@gmail.com>
2015-05-26 12:55:24 -0400

    Adding Initial Project

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace BeehiveManagementSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.nextShift = new System.Windows.Forms.Button();
+            this.workerBeeJob = new System.Windows.Forms.ComboBox();
+            this.shifts = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.shifts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 120);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(415, 162);
+            this.textBox1.TabIndex = 0;
+            // 
+            // nextShift
+            // 
+            this.nextShift.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nextShift.Location = new System.Drawing.Point(279, 18);
+            this.nextShift.Name = "nextShift";
+            this.nextShift.Size = new System.Drawing.Size(147, 91);
+            this.nextShift.TabIndex = 1;
+            this.nextShift.Text = "Work The Next Shift";
+            this.nextShift.UseVisualStyleBackColor = true;
+            this.nextShift.Click += new System.EventHandler(this.nextShift_Click);
+            // 
+            // workerBeeJob
+            // 
+            this.workerBeeJob.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.workerBeeJob.FormattingEnabled = true;
+            this.workerBeeJob.Items.AddRange(new object[] {
+            "Baby Bee Tutoring",
+            "Egg Care",
+            "Hive Maintenance ",
+            "Honey Manufacturing",
+            "Nectar Collector",
+            "Sting Patrol"});
+            this.workerBeeJob.Location = new System.Drawing.Point(36, 56);
+            this.workerBeeJob.Name = "workerBeeJob";
+            this.workerBeeJob.Size = new System.Drawing.Size(121, 21);
+            this.workerBeeJob.TabIndex = 2;
+            // 
+            // shifts
+            // 
+            this.shifts.Location = new System.Drawing.Point(162, 56);
+            this.shifts.Name = "shifts";
+            this.shifts.Size = new System.Drawing.Size(96, 20);
+            this.shifts.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(34, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(224, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Assign This Job To A Bee";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Job";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(162, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Shifts";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 18);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(261, 96);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Worker Bee Assignments";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 294);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.shifts);
+            this.Controls.Add(this.workerBeeJob);
+            this.Controls.Add(this.nextShift);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Beehive Management System";
+            ((System.ComponentModel.ISupportInitialize)(this.shifts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button nextShift;
+        private System.Windows.Forms.ComboBox workerBeeJob;
+        private System.Windows.Forms.NumericUpDown shifts;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Queen.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeehiveManagementSystem
+{
+    class Queen
+    {
+        //Declare Fields
+
+        public static Worker[] workers { get; set; }
+        private static int shiftNumber { get; set; }
+
+        //Setup Object Constructor 
+
+        public Queen(Worker[] Workers)
+        {
+            workers = Workers; 
+        }
+
+        //Declare Methods
+
+        public bool AssignWork(string job, int numberOfShifts)
+        {
+
+            foreach (Worker bee in workers)
+            {
+                if (String.IsNullOrEmpty(bee.CurrentJob))
+                {
+                    bool doableJob = bee.DoThisJob(job, numberOfShifts);
+                    if(doableJob == true)
+                         return true;
+
+                }
+
+            }
+            return false;
+        }
+
+// Hooks into the forms "work the next shift button". Tells each worker object to work one shift
+// and then checks the status so that it can add a line to the shift report. 
+      
+        public static string WorkTheNextShift()
+        {
+ 
+            shiftNumber++;
+             string report;
+
+            report = String.Format("Report for shift# {0} \r\n " , shiftNumber);
+            foreach (Worker bee in workers)
+            {
+                int index = Array.IndexOf(workers, bee);
+                bee.DidYouFinish(bee);
+                if (bee.DidYouFinish(bee) == false)
+                {
+                    // update status report that bee is still working
+                    // and how many shifts are left. Include which bee
+                    // it is we are referencing
+                   
+                   report += String.Format("Worker# {0} will be done with {1} after {2} shifts \r\n ", index, bee.CurrentJob, bee.ShiftsLeft);
+                 
+                }
+                if(bee.DidYouFinish(bee) == true)
+                {
+                    // update status report that bee# is finished jobname
+                    // and is ready for next job.
+                    report += String.Format("Worker#{0} is finished the job \r\n " +
+                        "Worker#{0} is not working \r\n ", index);
+                    
+                   // string noJob = String.Format("Worker#{0} is not working", index);
+                }
+            }
+            return report;
+        }
+        //Remember typical naming convention for private field is _private 
+    }
+
+   public class Worker
+     {
+         //Declare Backing Fields
+         private string currentJob;
+         private int shiftsLeft;
+         private int shiftsToWork;
+         private int shiftsWorked;
+         private string[] jobsICanDo;
+
+        //Declare Public Properties    
+         public string CurrentJob { get { return currentJob; } }
+         
+        public int ShiftsLeft { get {return shiftsLeft; } }
+
+        public int ShiftsToWork { get { return shiftsToWork; }}
+
+        public int ShiftsWorked { get; set; }
+       
+      //Setup Object Constructor
+
+        public Worker(string[] JobsICanDo)
+        {
+            this.jobsICanDo = JobsICanDo;
+        }
+
+      //Declare Methods
+
+       // The Queen uses this method to assign a new job to the workers
+       // if the worker is already doing the job it returns false. If
+       // the worker is not doing the job, it checks to see if it is a 
+       // job the worker can do. If it is, it breaks the for loop and 
+       // sets the shiftsToWork field to the value passed to the 
+       // numberOfShifts parameter. Then it sets the currentJob field 
+       // to the value of the job parameter. It resets shiftsWorked to 
+       // zero. Then it returns true.
+
+
+        public bool DoThisJob(string job, int numberOfShifts)
+        {
+            if (job != currentJob) //Alternative if(!job.Equals(currentJob), checks if current job is already being done.
+            {
+                for (int x = 0; x <= jobsICanDo.Length - 1; x++)
+                    if (job == jobsICanDo[x]) 
+                        break; 
+                    {
+                        this.shiftsToWork = numberOfShifts; //remember to use this keyword for props
+                        currentJob = job;
+                        shiftsWorked = 0;
+                     return true;   
+                    }
+            }
+                return false; 
+            }
+
+       // Worker works a shift and then checks to see if it has any shifts
+       // left to work. If it does not, it sets currentJob to null so it 
+       // can accept the next job. Otherwise it returns false so it can 
+       // continue working.
+
+        public bool DidYouFinish(Worker workers)
+        {
+            shiftsToWork--;
+            if (shiftsToWork == 0)
+            {
+                currentJob = null;
+                return true;
+            }
+            else
+                return false;
+        } 
+        }
+       
+    }     
+  
+
+
